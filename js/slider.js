ada9f0e34d8c698d9d7f2c0dd0af0fcf4c5b51c5
SukhOPGod <sukhbircing@gmail.com>
2022-08-24 17:31:30 +0530

    review page done

--- js/slider.js
+++ js/slider.js
@@ -8,4 +8,13 @@ $(function() {
         autoplay: true,
         dots: true
     });
+    $('.more_detail').on('click', ()=> {
+        overlayToggle();
+    });
+    $('.overlay_close').on('click', ()=> {
+        overlayToggle();
+    });
+    function overlayToggle() {
+        $('.detail_overlay').toggleClass('active');
+    }
 });
