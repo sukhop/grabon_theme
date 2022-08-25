8eacc42d1fbd600d46151960730c77c2ead64a76
SukhOPGod <sukhbircing@gmail.com>
2022-08-25 12:59:08 +0530

    notification and search popup created

--- js/script.js
+++ js/script.js
@@ -54,3 +54,43 @@ scrollTopBtn.addEventListener('click', ()=> {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
+
+// notification popup
+let notifyIcon = document.querySelector('.notification');
+let notifyWrapper = document.querySelector('.notify_wrapper');
+let notifyOverlay = document.querySelector('.notify-overlay');
+let notifyClose = document.querySelector('.notify_close');
+
+notifyIcon.addEventListener('click', ()=> {
+    notifyToggle();
+});
+notifyOverlay.addEventListener('click', ()=> {
+    notifyToggle();
+});
+notifyClose.addEventListener('click', ()=> {
+    notifyToggle();
+});
+function notifyToggle() {
+    notifyWrapper.classList.toggle('notify_active');
+    notifyOverlay.classList.toggle('visible');
+}
+
+// search popup
+let searchBtn = document.querySelector('.search');
+let searchWrapper = document.querySelector('.search_wrapper');
+let searchClose = document.querySelector('.close_search');
+let searchOverlay = document.querySelector('.search_overlay');
+
+searchBtn.addEventListener('click', ()=> {
+    searchToggle();
+});
+searchClose.addEventListener('click', ()=> {
+    searchToggle();
+});
+searchOverlay.addEventListener('click', ()=> {
+    searchToggle();
+});
+function searchToggle() {
+    searchWrapper.classList.toggle('search_active');
+    searchOverlay.classList.toggle('search_active');
+}
